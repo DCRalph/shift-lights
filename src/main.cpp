584c966ac8a85f724208d0d1f12e05ab1d74860a
Technoblade <william@williamgiles.co.nz>
2024-05-19 00:53:38 +1200

    working

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,119 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <Adafruit_NeoPixel.h>
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define LED_PIN 6
+#define LED_COUNT 8
+
+Adafruit_NeoPixel pixels(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
+
+int onRPM = 20;
+int normMaxRPM = 800;
+int overRevRPM = 1000;
+
+bool doneStartup = false;
+
+const uint32_t tachColor[LED_COUNT] = {
+    Adafruit_NeoPixel::Color(0, 255, 0), // green
+    Adafruit_NeoPixel::Color(0, 255, 0),
+    Adafruit_NeoPixel::Color(0, 255, 0),
+    Adafruit_NeoPixel::Color(0, 255, 0),
+    Adafruit_NeoPixel::Color(255, 255, 0),
+    Adafruit_NeoPixel::Color(255, 255, 0),
+    Adafruit_NeoPixel::Color(255, 0, 0), // orange
+    Adafruit_NeoPixel::Color(255, 0, 0),
+};
+
+const uint32_t shiftColor = Adafruit_NeoPixel::Color(0, 0, 255);            // blue
+const uint32_t overRevColor = Adafruit_NeoPixel::Color(255, 255, 255, 255); // red
+
+// Set the RPMuency when each LED should turn on
+// First LED turns on at minRPMuency
+int lightShiftRPM[LED_COUNT] = {
+    50,
+    100,
+    200,
+    300,
+    400,
+    500,
+    600,
+    700,
+};
+
+void setup()
+{
+  Serial.begin(9600);
+
+  pixels.begin();
+  pixels.show();
+  pixels.setBrightness(255);
+
+  pinMode(A0, INPUT);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  int RPM = analogRead(A0);
+  Serial.println(RPM);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (doneStartup == false)
+  {
+    if (RPM > onRPM)
+    {
+      // run start sequence
+      // LEDs will light up, flash and light out upon starting the engine.
+      for (int i = 0; i < LED_COUNT; ++i)
+      {
+        pixels.setPixelColor(i, tachColor[i]);
+        pixels.show();
+        delay(80);
+      }
+      for (int i = 0; i < LED_COUNT; ++i)
+      {
+        pixels.setPixelColor(i, tachColor[i]);
+        pixels.show();
+      }
+      for (int i = LED_COUNT - 1; i >= 0; --i)
+      {
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+        pixels.show();
+        delay(80);
+      }
+      doneStartup = true;
+      pixels.fill(pixels.Color(0, 0, 0));
+      pixels.show();
+    }
+    if (RPM < onRPM)
+      doneStartup = false;
+  }
+
+  if (RPM < normMaxRPM) // normal operating range
+  {
+    for (int i = 0; i < LED_COUNT; ++i)
+    {
+      if (RPM > lightShiftRPM[i])
+        pixels.setPixelColor(i, tachColor[i]);
+      else
+        pixels.setPixelColor(i, pixels.Color(0, 0, 0));
+    }
+    pixels.show();
+  }
+  else if (RPM >= normMaxRPM && RPM < overRevRPM) // shift flash
+  {
+    pixels.fill(shiftColor);
+    pixels.show();
+    delay(30);
+    pixels.fill(pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(30);
+  }
+  else if (RPM >= overRevRPM) // over rev flash
+  {
+    pixels.fill(overRevColor);
+    pixels.show();
+    delay(30);
+    pixels.fill(pixels.Color(0, 0, 0));
+    pixels.show();
+    delay(30);
+  }
 }
