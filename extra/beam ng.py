5eca39fc192dbb95285e1f69e66c8c04c684a985
Technoblade <william@williamgiles.co.nz>
2025-02-28 13:26:20 +1300

    Update beam ng.py

--- extra/beam ng.py
+++ extra/beam ng.py
@@ -9,11 +9,15 @@ struct_format = "I4sHBcffffffIIf fff16s16si"
 UDP_IP = "127.0.0.1"
 UDP_PORT = 4444
 
+UDP_IP_ADDRESS = "10.99.20.20"
+UDP_PORT_NO = 21324
+NUM_PIXELS = 120
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 sock.setblocking(False)
 
-serial_port = serial.Serial("COM16", 115200, timeout=0)
+serial_port = serial.Serial("COM7", 115200, timeout=0)
 
 # parse data using the following schema
 # unsigned       time;            // time in milliseconds (to check order) // N/A, hardcoded to 0
@@ -36,6 +40,73 @@ serial_port = serial.Serial("COM16", 115200, timeout=0)
 # char           display1[16];    // Usually Fuel // N/A, hardcoded to ""
 # char           display2[16];    // Usually Settings // N/A, hardcoded to ""
 # int            id;              // optional - only if OutGauge ID is specified
+
+
+def send(msg):
+    bytesToSend = bytes([2, 2] + msg)
+    clientSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    clientSock.sendto(bytesToSend, (UDP_IP_ADDRESS, UDP_PORT_NO))
+
+
+num_leds = 120
+colors_per_led = 3
+
+# Create an array to represent the LED colors
+led_array = [0] * (num_leds * colors_per_led)
+
+# Define the colors for the shift lights
+shift_light_colors = [(0, 255, 0), (255, 150, 0), (255, 0, 0)]  # Green  # Yellow  # Red
+
+tachColor = []
+
+for i in range(0, num_leds):
+    if i < num_leds / 2:
+        tachColor.append(shift_light_colors[0])
+    elif i < num_leds * 3 / 4:
+        tachColor.append(shift_light_colors[1])
+    else:
+        tachColor.append(shift_light_colors[2])
+
+
+lightShiftRPM = []
+
+for i in range(0, num_leds):
+    # from 1000 to 6000 over the 120 leds
+    lightShiftRPM.append(1000 + (5000 / num_leds) * i)
+
+
+def set_led_color(led_index, red, green, blue):
+    start_index = led_index * colors_per_led
+    led_array[start_index] = min(255, max(0, red))
+    led_array[start_index + 1] = min(255, max(0, green))
+    led_array[start_index + 2] = min(255, max(0, blue))
+
+
+def update_shift_lights(rpm):
+    global limiter_flash
+
+    if rpm > 7500:
+        if time.time() % 0.1 > 0.05:
+            for i in range(num_leds):
+                set_led_color(i, 255, 0, 0)
+        else:
+            for i in range(num_leds):
+                set_led_color(i, 0, 0, 0)
+
+        pass
+    elif rpm > 7000:
+        if time.time() % 0.1 > 0.05:
+            for i in range(num_leds):
+                set_led_color(i, 0, 0, 255)
+        else:
+            for i in range(num_leds):
+                set_led_color(i, 0, 0, 0)
+    else:
+        for i in range(num_leds):
+            if rpm > lightShiftRPM[i]:
+                set_led_color(i, *tachColor[i])
+            else:
+                set_led_color(i, 0, 0, 0)
 
 
 def parseData(data):
@@ -73,8 +144,10 @@ def parseData(data):
 
     rpmString = f"{parsed_struct['rpm']}\n"
     serial_port.write(rpmString.encode("utf-8"))
-    # serial_port.flush()
     serial_port.read_all()
+
+    # update_shift_lights(parsed_struct["rpm"])
+    # send(led_array)
 
 
 start_time = time.time()
